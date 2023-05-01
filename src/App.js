bcadd1966a422c4f74139b0c5f80e03906a6ce81
Adrián Montemayor Rojas <adrianmontemayor.r@gmail.com>
2023-04-30 23:59:12 -0600

    Navbar Complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./App.css";
+import React from "react";
+import Navbar from "./Components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Components/Pages/Home";
+import Generator from "./Components/Pages/Generator";
+import Saved from "./Components/Pages/Saved";
 
 function App() {
   return (
-    <div>
-      <p>Hello</p>
-    </div>
+    <React.Fragment>
+      <Router>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/generator" element={<Generator />} />
+            <Route path="/saved" element={<Saved />} />
+          </Routes>
+        </div>
+      </Router>
+    </React.Fragment>
   );
 }
 
---
+++ src/Components/Navbar.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { FaBars, FaTimes, FaPaintBrush } from "react-icons/fa";
+import "./Navbar.css";
+import { NavLink } from "react-router-dom";
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
+  return (
+    <React.Fragment>
+      <nav className="navbar">
+        <div className="nav-container">
+          <NavLink exact to="/" className="nav-logo">
+            Color Palette Generator
+            <FaPaintBrush style={{ height: 20, marginLeft: 5 }} />
+          </NavLink>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <NavLink exact to="/" className="nav-links" onClick={handleClick}>
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/generator"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Generator
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/saved"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Saved
+              </NavLink>
+            </li>
+          </ul>
+          <div className="nav-icon" onClick={handleClick}>
+            {click ? <FaTimes /> : <FaBars />}
+          </div>
+        </div>
+      </nav>
+    </React.Fragment>
+  );
+}
+
+export default Navbar;
